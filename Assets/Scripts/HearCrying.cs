7a665275851ea1ac5630852ef231b62eb2648309
jseaton <jseaton612@gmail.com>
2020-03-16 16:04:38 -0400

    Rough Stage 1 done

---
+++ Assets/Scripts/HearCrying.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class HearCrying : MonoBehaviour
+{
+    bool awake = false;
+    bool synced = false;
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (!awake && Input.GetKey("f"))
+        {
+            animator.SetTrigger("WakeUp");
+            awake = true;
+            FindObjectOfType<ProgressBar1>().trust++;
+        }
+
+        if (awake && animator.GetCurrentAnimatorStateInfo(0).IsName("WakeUp"))
+        {
+            synced = true;
+        }
+
+        if (synced && animator.GetCurrentAnimatorStateInfo(0).IsName("Asleep"))
+        {
+            awake = false;
+            synced = false;
+        }
+    }
+}
---
+++ Assets/Scripts/ProgressBar1.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar1 : MonoBehaviour
+{
+    public int trust = 0;
+    Slider trustBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trustBar = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        trustBar.value = trust;
+    }
+}
