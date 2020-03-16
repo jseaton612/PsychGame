45a50ec021204649d63248114f30f9d4832e835a
jseaton <jseaton612@gmail.com>
2020-03-16 12:46:50 -0400

    Camera Working?

---
+++ Assets/RotateCameraWithMouse.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class RotateCameraWithMouse : MonoBehaviour
+{
+    public float sensitivity = 1f;
+    void Update()
+    {
+        float VRot = Input.GetAxis("Mouse Y");
+        transform.Rotate(-VRot, 0, 0);
+    }
+}
---
+++ Assets/RotatePlayerWithMouse.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class RotatePlayerWithMouse : MonoBehaviour
+{
+    public float sensitivity = 1f;
+    void Update()
+    {
+        float HRot = Input.GetAxis("Mouse X");
+        transform.Rotate(0, HRot, 0);
+    }
+}
