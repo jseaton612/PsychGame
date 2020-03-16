190beccd5114df8d721cd3cc6004331a9dcfb2be
jseaton <jseaton612@gmail.com>
2020-03-16 10:49:21 -0400

    Camera Follow
    Next get it to move

---
+++ Assets/Follow.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position;
+    }
+}
