f6d49aa52024c5ba6d05977af6d62e286d6bc3f3
jseaton <jseaton612@gmail.com>
2020-03-17 01:47:55 -0400

    close

---
+++ Assets/Scripts/PlayerShift.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class PlayerShift : MonoBehaviour
+{
+    public float step = .1f;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Translate(Input.GetAxis("Horizontal") * step, 0, 0);
+    }
+}
