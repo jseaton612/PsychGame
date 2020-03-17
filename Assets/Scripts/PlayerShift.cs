8b0afd71756491ab8407c04728e600f5d8f3cf5b
jseaton <jseaton612@gmail.com>
2020-03-17 02:56:04 -0400

    testing

--- Assets/Scripts/PlayerShift.cs
+++ Assets/Scripts/PlayerShift.cs
@@ -2,10 +2,23 @@
 
 public class PlayerShift : MonoBehaviour
 {
-    public float step = .1f;
-    // Update is called once per frame
+    public float factor = .1f;
+    public float step = .08f;
+    public Rigidbody2D pivot;
+
+    private void Start()
+    {
+        pivot = GetComponent<Rigidbody2D>();
+    }
     void FixedUpdate()
     {
-        transform.Translate(Input.GetAxis("Horizontal") * step, 0, 0);
+        if (Failures.failed2)
+        {
+            pivot.AddForce(new Vector2(Input.GetAxis("Horizontal") * factor, 0));
+        }
+        else
+        {
+            transform.Translate(Input.GetAxis("Horizontal") * step, 0, 0);
+        }
     }
 }
