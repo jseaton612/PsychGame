cadff6c6eb61ee7fc4ce441aed77e65391208287
jseaton <jseaton612@gmail.com>
2020-03-17 03:19:24 -0400

    release candidate
    stage 1 still kinda bright

--- Assets/Scripts/GameManager2.cs
+++ Assets/Scripts/GameManager2.cs
@@ -4,10 +4,19 @@ using UnityEngine.UI;
 public class GameManager2 : MonoBehaviour
 {
     public int timeRequired = 30;
+    public GameObject hint;
     public Text timer;
     public GameObject balance;
     public Text bText;
     public GameObject finalPanel;
+
+    public void Start()
+    {
+        if (Failures.failed2)
+        {
+            hint.SetActive(true);
+        }
+    }
 
     public void Update()
     {
@@ -17,13 +26,17 @@ public class GameManager2 : MonoBehaviour
         if (remainingTime < 0 && balance.transform.position.y > -5)
         {
             bText.text = "You won!\nWell Played";
+            hint.SetActive(false);
             finalPanel.SetActive(true);
+            return;
         }
 
         else if (balance.transform.position.y < -5)
         {
             bText.text = "GAME OVER\nThanks for playing!";
+            hint.SetActive(false);
             finalPanel.SetActive(true);
+            return;
         }
     }
 }
